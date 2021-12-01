485fb611b03bccccd234aad53c587c6a8b21604f
MackKaputo <mackkaputo@gmail.com>
2021-12-01 15:33:46 +0200

    input validation completed

--- server/src/index.ts
+++ server/src/index.ts
@@ -2,8 +2,10 @@ import express from "express"
 import authRoutes from "./routes/auth"
 
 const app = express()
+app.use(express.json())
 
 app.use("/auth", authRoutes)
+
 
 app.listen(8080, () => {
     console.log(`App listening on port 8080`)
--- server/src/routes/auth.ts
+++ server/src/routes/auth.ts
@@ -1,9 +1,32 @@
 import express from "express"
+import { body, validationResult } from "express-validator"
 
 const router = express.Router()
 
-router.post('/signup', async (req, res) => {
-    res.send("Signup route")
+
+router.post(
+    '/signup',
+    body("email").isEmail().withMessage("The email is invalid"),
+    body("password").isLength({min: 5}).withMessage("The password's length should be at least 5 characters"),
+    async (req, res) => {
+
+        const validationErrors = validationResult(req)
+
+        if(!validationErrors.isEmpty()) {
+            const errors = validationErrors.array().map(error => {
+                return {
+                    message: error.msg
+                }
+            })
+
+            return res.json({errors})
+        }
+
+        const { email, password } = req.body
+
+        
+    res.json({email, password})
 })
+
 
 export default router
